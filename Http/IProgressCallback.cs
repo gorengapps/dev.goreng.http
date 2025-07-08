e04cca4d4b41f447ed72a2797c3edefffab96296
Jason Meulenhof <j.meulenhoff@me.com>
2025-07-08 11:31:00 +0200

    Update code style

--- Http/IProgressCallback.cs
+++ Http/IProgressCallback.cs
@@ -10,23 +10,23 @@ namespace Http
         /// <summary>
         /// The number of bytes downloaded so far.
         /// </summary>
-        public ulong BytesDownloaded { get; }
+        public ulong bytesDownloaded { get; }
         
         /// <summary>
         /// The total number of bytes to download. May be 0 if unknown.
         /// </summary>
-        public ulong TotalBytes { get; }
+        public ulong totalBytes { get; }
         
         /// <summary>
         /// The download progress as a percentage (0.0 to 1.0). 
         /// Returns 0 if total bytes is unknown.
         /// </summary>
-        public float Progress => TotalBytes > 0 ? (float)BytesDownloaded / TotalBytes : 0f;
+        public float progress => totalBytes > 0 ? (float)bytesDownloaded / totalBytes : 0f;
 
         public DownloadProgress(ulong bytesDownloaded, ulong totalBytes)
         {
-            BytesDownloaded = bytesDownloaded;
-            TotalBytes = totalBytes;
+            this.bytesDownloaded = bytesDownloaded;
+            this.totalBytes = totalBytes;
         }
     }
 
